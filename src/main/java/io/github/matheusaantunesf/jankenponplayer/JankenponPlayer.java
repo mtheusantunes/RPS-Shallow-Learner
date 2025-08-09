04490494fe8088ba99d9a61c7a25a0738f0c5d2f
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2025-08-09 10:27:23 -0300

    Plano de ataque apos conseguir amostras

--- src/main/java/io/github/matheusaantunesf/jankenponplayer/JankenponPlayer.java
+++ src/main/java/io/github/matheusaantunesf/jankenponplayer/JankenponPlayer.java
@@ -16,11 +16,11 @@ import java.util.Comparator;
 public class JankenponPlayer
         extends AbstractPlayer {
 
-    Integer rodada = 1, ultimoMovimento, quantidadeRock = 0, quantidadePaper = 0,
-            quantidadeScissors = 0;
-    ArrayList<ArrayList<Move>> amostras;
+    Integer rodada = 0, ultimoMovimento = -1,
+            quantidadeRock = 0, quantidadePaper = 0, quantidadeScissors = 0;
+    ArrayList<ArrayList<Move>> amostras = new ArrayList<>();
     Integer[][] menosProvaveis = new Integer[3][3];
-    ArrayList<ArrayList<ProporcaoIndice>> proporcoes;
+    ArrayList<ArrayList<ProporcaoIndice>> proporcoes = new ArrayList<>();
 
     @Override
     public String getDeveloperName() {
@@ -29,20 +29,23 @@ public class JankenponPlayer
 
     @Override
     public Move makeMyMove(Move opponentPreviousMove) {
+        rodada++;
         if (opponentPreviousMove == Move.NONE) {
             rodada = 1;
             quantidadeRock = quantidadePaper = quantidadeScissors = 0;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    proporcoes.clear();
-                    amostras.clear();
-                    ultimoMovimento = 0;
-                    return Move.ROCK;
-                }
-            }
-        } else if (rodada < 31) {
+            proporcoes.clear();
+            amostras.clear();
+            ultimoMovimento = 0;;
+            return Move.ROCK;
+        }
+        if (rodada < 32) {
             Integer numeroAleatorio = ThreadLocalRandom.current().nextInt(0, 3);
-            amostras.get(ultimoMovimento).add(opponentPreviousMove);
+            if (ultimoMovimento > -1) {
+                while (amostras.size() <= ultimoMovimento) {
+                    amostras.add(new ArrayList<>());
+                }
+                amostras.get(ultimoMovimento).add(opponentPreviousMove);
+            }
             ultimoMovimento = numeroAleatorio;
             if (numeroAleatorio == 0) {
                 return Move.ROCK;
@@ -52,34 +55,47 @@ public class JankenponPlayer
                 return Move.SCISSORS;
             }
         } else {
-            if (rodada == 31) {
+            if (rodada == 32) {
                 for (int i = 0; i < amostras.size(); i++) {
+                    quantidadeRock = quantidadePaper = quantidadeScissors = 0;
                     for (Move movimentoOponente : amostras.get(i)) {
                         if (movimentoOponente == Move.ROCK) {
                             quantidadeRock++;
                         } else if (movimentoOponente == Move.PAPER) {
                             quantidadePaper++;
+
                         } else {
                             quantidadeScissors++;
                         }
                     }
-                    proporcoes.get(i).add(new ProporcaoIndice((float) 
-                            quantidadeRock / amostras.get(i).size(), 0));
-                    proporcoes.get(i).add(new ProporcaoIndice((float) 
-                            quantidadePaper / amostras.get(i).size(), 1));
-                    proporcoes.get(i).add(new ProporcaoIndice((float) 
-                            quantidadeScissors / amostras.get(i).size(), 2));
-                    proporcoes.get(i).sort(Comparator.comparing
-                            (ProporcaoIndice::proporcao));
+                    while (proporcoes.size() < amostras.size()) {
+                        proporcoes.add(new ArrayList<>());
+                    }
+                    
+                    proporcoes.get(i).add(new ProporcaoIndice((float) quantidadeRock / amostras.get(i).size(), 0));
+                    proporcoes.get(i).add(new ProporcaoIndice((float) quantidadePaper / amostras.get(i).size(), 1));
+                    proporcoes.get(i).add(new ProporcaoIndice((float) quantidadeScissors / amostras.get(i).size(), 2));
+                    proporcoes.get(i).sort(Comparator.comparing(ProporcaoIndice::proporcao));
+
                 }
             }
 
+            Integer previsao = proporcoes.get(ultimoMovimento).get(2).indice();
+
+            if (previsao == 0) {
+                ultimoMovimento = 1;
+                return Move.PAPER;
+            } else if (previsao == 1) {
+                ultimoMovimento = 2;
+                return Move.SCISSORS;
+            } else {
+                ultimoMovimento = 0;
+                return Move.ROCK;
+            }
         }
-        return Move.NONE;
     }
 
     public record ProporcaoIndice(Float proporcao, Integer indice) {
 
     }
-
 }
